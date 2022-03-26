bb90d8ebdd6f7ed4625f391c4ad69fe53627c8c2
datngo <dat.ngotri.ncc@gmail.com>
2022-03-26 14:34:41 +0700

    add gateway & load balancing

--- fraud/src/main/java/com/datngo/fraud/model/FraudCheckResponse.java
+++ fraud/src/main/java/com/datngo/fraud/model/FraudCheckResponse.java
@@ -1,10 +1,12 @@
 package com.datngo.fraud.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class FraudCheckResponse {
     private boolean isFraudulentCustomer;
 }
